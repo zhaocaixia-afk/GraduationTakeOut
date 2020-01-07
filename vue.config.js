1026a0ba0a329cb890788c25c381129a71985483
赵采霞 <121357625@qq.com>
2020-01-07 17:25:19 +0800

    浏览器和路径别名设置

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+// 文件别名
+const path = require('path')
+function resolve(dir){
+    return path.join(__dirname,dir)
+}
+
+module.exports = {
+    // 浏览器配置
+    devServer:{
+        port:8080,
+        host:'localhost',
+        open:true
+    },
+    // 文件别名
+    chainWebpack:(config) => {
+        config.resolve.alias
+        .set('@',resolve('./src'))
+        .set('assets',resolve('./src/assets'))
+        .set('components',resolve('./src/components'))
+        .set('router',resolve('./src/router'))
+        .set('store',resolve('./src/store'))
+        .set('views',resolve('./src/views'))
+    }
+}
